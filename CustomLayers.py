f86ed7feb12c4a1e52363521aca9166bb0191353
克勞德 <masatada67890@gmail.com>
2022-08-20 12:16:51 +1000

    Add position and batch boardcast to latent array

--- CustomLayers.py
+++ CustomLayers.py
@@ -5,17 +5,23 @@
 import os
 # Suppress the INFO message
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+from PositionEncodings import learnable_pos_embedding
 import tensorflow as tf
 
 
 class LatentArray(tf.keras.layers.Layer):
-    def __init__(self, latent_num, proj_dim):
+    def __init__(self, batch_size, latent_num, proj_dim):
         super(LatentArray, self).__init__()
+        self.batch_size = batch_size
         self.latent_num = latent_num
         self.proj_dim = proj_dim
         self.latent_array = self.add_weight(
             shape=(self.latent_num, self.proj_dim), initializer="random_normal", trainable=True
         )
+        self.pos_embedding = learnable_pos_embedding(pos_num=self.latent_num, proj_dim=self.proj_dim)
 
     def generate(self):
-        return self.latent_array
+        latent_array_wpos = self.latent_array + self.pos_embedding
+        latent_array_wpos = tf.broadcast_to(latent_array_wpos, [self.batch_size] + latent_array_wpos.shape)
+
+        return latent_array_wpos
